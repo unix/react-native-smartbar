552bb4514ee42b70ef72c47649ee58114f484c3a
WittBulter <nanazuimeng123@gmail.com>
2016-09-07 18:09:08 +0800

    add example

---
+++ example/list.js
@@ -0,0 +1,22 @@
+/**
+ * Created by WittBulter on 16/9/7.
+ */
+
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+
+export default class List extends Component {
+	// 构造
+	constructor (props){
+		super(props)
+		// 初始状态
+		this.state = {}
+	}
+	render (){
+		return (
+			<View>
+				<Text>list page</Text>
+			</View>
+		)
+	}
+}
